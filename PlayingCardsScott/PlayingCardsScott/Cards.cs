45096ddd4c53114787f0252fe1d95eeaa0e1f892
scottpallo <scott.pallo@gmail.com>
2013-09-26 14:32:49 -0700

    Playing Cards Project

---
+++ PlayingCardsScott/PlayingCardsScott/Cards.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace PlayingCardsScott
+{
+    internal enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+
+    internal enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+
+    internal class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Enum.GetName(typeof(Values), Value) + " of " + Enum.GetName(typeof(Suits), Suit);
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ PlayingCardsScott/PlayingCardsScott/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingCardsScott
+{
+    internal class Program   
+    {
+        private static void Main(string[] args)
+        {
+            Card[] cards = new Card[52];
+            int index = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Suits suit = (Suits) Enum.Parse(typeof (Suits), i.ToString());
+
+                for (int j = 1; j <= 13; j++)
+                {
+                    Values value = (Values) Enum.Parse(typeof (Values), j.ToString());
+
+                    cards[index] = new Card(suit, value);
+                    index++;
+                }
+            }
+
+            Random rng = new Random();
+            int[] indexesUsed = new int[] {-1, -1, -1, -1, -1};
+            Card[] hand = new Card[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                bool isUnique = false;
+
+                while (!isUnique)
+                {
+                    int cardToDraw = rng.Next(0, 52);
+                    if (!indexesUsed.Contains(cardToDraw))
+                    {
+                        isUnique = true;
+                        hand[i] = cards[cardToDraw];
+                        indexesUsed[i] = cardToDraw;
+                    }
+                }             
+            }
+
+            foreach (Card c in hand)
+            {
+                Console.WriteLine(c.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+    
+
